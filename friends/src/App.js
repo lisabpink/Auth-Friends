2cfbdac6860e19482f7eba07f5068e122e0fe3ce
LISA_COOK <lisabpink@gmail.com>
2020-01-29 11:01:07 -0500

    LDC axios call working

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <LoginForm/>
+     <Route exact path = '/' component={LoginForm}/> 
 
   </div>
   );
